a3c4b9b2c6d7bac7edb27804d7d5641e4e50c97b
Jaren Lynch <Jaren Lynch>
2017-04-30 19:13:21 -0700

    Adds card class

--- app/src/main/java/com/example/android/judge/Search/Card.java
+++ app/src/main/java/com/example/android/judge/Search/Card.java
@@ -5,9 +5,45 @@ package com.example.android.judge.Search;
  */
 
 public class Card {
-    String name;
-    String colors;
-    String type;
-    String rarity;
-    String image;
+    public String mName;
+    public String mColors;
+    public String mType;
+    public String mRarity;
+    public String mImage;
+    public String mText;
+
+    public Card(String name, String color, String type, String rarity, String image, String text ) {
+        mName = name;
+        mColors = color;
+        mType = type;
+        mRarity = rarity;
+        mImage = image;
+        mType = text;
+    }
+
+    public String getName() {
+        return mName;
+    }
+
+    public String getColors() {
+        return mColors;
+    }
+
+    public String getType() {
+        return mType;
+    }
+
+    public String getRarity() {
+        return mRarity;
+    }
+
+    public String getImage() {
+        return mImage;
+    }
+
+    public String getText() {
+        return mText;
+    }
 }
+
+
