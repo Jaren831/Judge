ac4bafbaa990ce43ecc2b482b7ea70cf4926541d
Jaren Lynch <Jaren Lynch>
2017-05-05 19:01:27 -0700

    Adds widget

--- app/src/main/java/com/example/android/judge/Widget/MatchWidgetProvider.java
+++ app/src/main/java/com/example/android/judge/Widget/MatchWidgetProvider.java
@@ -10,6 +10,7 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Build;
 import android.support.annotation.NonNull;
+import android.util.Log;
 import android.widget.RemoteViews;
 
 import com.example.android.judge.MainActivity;
@@ -22,27 +23,39 @@ import java.util.Random;
  */
 public class MatchWidgetProvider extends AppWidgetProvider {
 
-    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
-                                int appWidgetId) {
-        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.match_widget);
-
-        // Set up the collection
-        setRemoteAdapter(context, views);
-        // Instruct the widget manager to update the widget
-        appWidgetManager.updateAppWidget(appWidgetId, views);
-    }
+    private static final String ACTION_CLICK = "ACTION_CLICK";
 
     @Override
-    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
-        // There may be multiple widgets active, so update all of them
-        for (int appWidgetId : appWidgetIds) {
-            updateAppWidget(context, appWidgetManager, appWidgetId);
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager,
+                         int[] appWidgetIds) {
+
+        // Get all ids
+        ComponentName thisWidget = new ComponentName(context,
+                MatchWidgetProvider.class);
+        int[] allWidgetIds = appWidgetManager.getAppWidgetIds(thisWidget);
+        for (int widgetId : allWidgetIds) {
+            // create some random data
+
+            RemoteViews remoteViews = new RemoteViews(context.getPackageName(),
+                    R.layout.match_widget);
+            // Set the text
+            remoteViews.setTextViewText(R.id.player1_life_widget, String.valueOf(20));
+            remoteViews.setTextViewText(R.id.player2_life_widget, String.valueOf(20));
+
+            // Register an onClickListener
+            Intent intent = new Intent(context, MatchWidgetProvider.class);
+            Intent MatchIntent = new Intent(context, MainActivity.class);
+
+
+            intent.setAction(AppWidgetManager.ACTION_APPWIDGET_UPDATE);
+            intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, appWidgetIds);
+
+            PendingIntent pendingIntent = PendingIntent.getActivity(context,
+                    0, MatchIntent, 0);
+            remoteViews.setOnClickPendingIntent(R.id.player1_life_widget, pendingIntent);
+            remoteViews.setOnClickPendingIntent(R.id.player2_life_widget, pendingIntent);
+
+            appWidgetManager.updateAppWidget(widgetId, remoteViews);
         }
-        super.onUpdate(context, appWidgetManager, appWidgetIds);
-    }
-    @SuppressWarnings("deprecation")
-    private static void setRemoteAdapter(Context context, @NonNull final RemoteViews views) {
-        views.setRemoteAdapter(0, R.id.widget_list,
-                new Intent(context, MatchWidgetService.class));
     }
 }
