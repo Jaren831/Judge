d5bf3417c48679f611c0f7ac7d3d0959b1da2998
Jaren Lynch <Jaren Lynch>
2017-05-06 17:52:02 -0700

    PDF now opens properly

--- app/src/main/java/com/example/android/judge/RuleBook/RuleBookFragment.java
+++ app/src/main/java/com/example/android/judge/RuleBook/RuleBookFragment.java
@@ -5,8 +5,10 @@ import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.View;
@@ -15,11 +17,14 @@ import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
+import com.example.android.judge.MainActivity;
 import com.example.android.judge.R;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
+
+import java.io.File;
 
 
 public class RuleBookFragment extends Fragment {
@@ -29,7 +34,9 @@ public class RuleBookFragment extends Fragment {
     View rootView;
     StorageReference basicStorageRef;
     StorageReference comprehensiveStorageRef;
-    ProgressBar progressBar;
+    String basicRuleFile;
+    String comprehensiveRuleFile;
+    private final String LOG_TAG = MainActivity.class.getSimpleName();
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -38,63 +45,45 @@ public class RuleBookFragment extends Fragment {
         rootView = inflater.inflate(R.layout.fragment_rule_book, container, false);
         setHasOptionsMenu(true);
 
-        progressBar = (ProgressBar) rootView.findViewById(R.id.rule_progress_bar);
-
         basicButton = (Button) rootView.findViewById(R.id.basic_button);
         comprehensiveButton = (Button) rootView.findViewById(R.id.comprehensive_button);
 
+        basicRuleFile = "MTG Basic Rules.pdf";
+        comprehensiveRuleFile = "MTG Comprehensive Rules.pdf";
+
         FirebaseStorage storage = FirebaseStorage.getInstance();
-        basicStorageRef = storage.getReferenceFromUrl("gs://judge-1b409.appspot.com/").child("MTG Basic Rules.pdf");
-        comprehensiveStorageRef = storage.getReferenceFromUrl("gs://judge-1b409.appspot.com/").child("MTG Comprehensive Rules.pdf");
+        basicStorageRef = storage.getReferenceFromUrl("gs://judge-1b409.appspot.com/").child(basicRuleFile);
+        comprehensiveStorageRef = storage.getReferenceFromUrl("gs://judge-1b409.appspot.com/").child(comprehensiveRuleFile);
 
         basicButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                progressBar.setVisibility(View.VISIBLE);
-                basicStorageRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
-                    @Override
-                    public void onSuccess(Uri uri) {
-                        progressBar.setVisibility(View.GONE);
-                        Toast.makeText(getContext(), uri.toString(), Toast.LENGTH_SHORT).show();
-                        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(uri.toString()));
-                        try {
-                            startActivity(browserIntent);
-                        } catch (ActivityNotFoundException e) {
-                            // Instruct the user to install a PDF reader here, or something
+                if (checksFileExistence(basicRuleFile)) {
+                    openPdfFile(basicRuleFile);
+                } else {
+                    basicStorageRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                        @Override
+                        public void onSuccess(Uri uri) {
+                            downloadPdfFile(uri);
                         }
-                    }
-                }).addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        Toast.makeText(getContext(), "FAIL", Toast.LENGTH_SHORT).show();
-
-                    }
-                });
+                    });
+                }
             }
         });
 
         comprehensiveButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                comprehensiveStorageRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
-                    @Override
-                    public void onSuccess(Uri uri) {
-                        progressBar.setVisibility(View.GONE);
-                        Toast.makeText(getContext(), uri.toString(), Toast.LENGTH_SHORT).show();
-                        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(uri.toString()));
-                        try {
-                            startActivity(browserIntent);
-                        } catch (ActivityNotFoundException e) {
-                            // Instruct the user to install a PDF reader here, or something
+                if (checksFileExistence(comprehensiveRuleFile)) {
+                    openPdfFile(comprehensiveRuleFile);
+                } else {
+                    comprehensiveStorageRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                        @Override
+                        public void onSuccess(Uri uri) {
+                            downloadPdfFile(uri);
                         }
-                    }
-                }).addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        Toast.makeText(getContext(), "FAIL", Toast.LENGTH_SHORT).show();
-
-                    }
-                });
+                    });
+                }
             }
         });
 
@@ -119,4 +108,33 @@ public class RuleBookFragment extends Fragment {
         super.onDetach();
     }
 
+    public boolean checksFileExistence(String fileName) {
+        File checkFile = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).getPath() + "/" + fileName);
+        return checkFile.exists();
+    }
+
+    public void openPdfFile(String file) {
+        File pdfFile = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).getPath() + File.separator +
+                file);
+        Uri path = Uri.fromFile(pdfFile);
+        Intent pdfOpenintent = new Intent(Intent.ACTION_VIEW);
+        pdfOpenintent.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+        pdfOpenintent.setDataAndType(path, "application/pdf");
+        Intent openWithIntent = Intent.createChooser(pdfOpenintent, "Open With");
+        try {
+            startActivity(openWithIntent);
+        } catch (ActivityNotFoundException e) {
+            Log.i(LOG_TAG, pdfFile.toString() + "start activity failed");
+        }
+    }
+
+    public void downloadPdfFile(Uri uri) {
+        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(uri.toString()));
+        try {
+            startActivity(browserIntent);
+        } catch (ActivityNotFoundException e) {
+            Toast.makeText(getContext(), R.string.pdf_broke, Toast.LENGTH_SHORT).show();
+        }
+
+    }
 }
--- app/src/main/java/com/example/android/judge/RuleBook/WebViewFragment.java
+++
@@ -1,8 +0,0 @@
-package com.example.android.judge.RuleBook;
-
-/**
- * Created by jaren on 5/6/2017.
- */
-
-public class WebViewFragment {
-}
