eb5ea1f817277d27f616ee12b89cc971a1fa55ca
Jaren Lynch <Jaren Lynch>
2017-05-08 13:09:20 -0700

    Adds contentprovider

--- app/src/main/java/com/app/android/judge/Data/MatchHistoryContract.java
+++ app/src/main/java/com/app/android/judge/Data/MatchHistoryContract.java
@@ -1,5 +1,6 @@
 package com.app.android.judge.Data;
 
+import android.net.Uri;
 import android.provider.BaseColumns;
 
 /**
@@ -7,15 +8,44 @@ import android.provider.BaseColumns;
  */
 
 public class MatchHistoryContract {
+
+    public static final String AUTHORITY = "com.app.android.judge";
+    public static final String PATH_MATCH = "match";
+    public static final String PATH_MATCH_WITH_ID = "match/*";
+    public static final Uri BASE_URI = Uri.parse("content://" + AUTHORITY);
+
+
     private MatchHistoryContract () {}
 
     public static final class MatchHistoryEntry implements BaseColumns {
 
         public final static String TABLE_NAME = "match_history";
+        public static final Uri URI = BASE_URI.buildUpon().appendPath(PATH_MATCH).build();
         public final static String _ID = BaseColumns._ID;
         public final static String COLUMN_PLAYER1_LIFE = "player1";
         public final static String COLUMN_PLAYER2_LIFE = "player2";
         public final static String COLUMN_PLAYER1_COLOR = "player1_color";
         public final static String COLUMN_PLAYER2_COLOR = "player2_color";
+        public static final int POSITION_ID = 0;
+        public static final int POSITION_PLAYER1_LIFE = 1;
+        public static final int POSITION_PLAYER2_LIFE = 2;
+        public static final int POSITION_PLAYER1_COLOR = 3;
+        public static final int POSITION_PLAYER2_COLOR = 4;
+        public static final String[] MATCH_COLUMNS = {
+                _ID,
+                COLUMN_PLAYER1_LIFE,
+                COLUMN_PLAYER2_LIFE,
+                COLUMN_PLAYER1_COLOR,
+                COLUMN_PLAYER2_COLOR
+        };
+
+        public static Uri makeUriForStock(String symbol) {
+            return URI.buildUpon().appendPath(symbol).build();
+        }
+
+        public static String getMatchromUri(Uri queryUri) {
+            return queryUri.getLastPathSegment();
+        }
+
     }
 }
